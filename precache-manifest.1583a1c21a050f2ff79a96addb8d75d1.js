1d3057a4498ea453f90b50d4dd6b9f113a219ae3
Dreamufor <missmengxue@gmail.com>
2020-03-29 23:20:57 +1300

    Updates

---
+++ precache-manifest.1583a1c21a050f2ff79a96addb8d75d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0bb7751c7ce7729d32761e2d113870",
+    "url": "/mmss-frontend/index.html"
+  },
+  {
+    "revision": "630c66d295ee7cf39405",
+    "url": "/mmss-frontend/static/css/2.8ed5c54c.chunk.css"
+  },
+  {
+    "revision": "943c3d7f6c4efb67480e",
+    "url": "/mmss-frontend/static/css/main.784eb61a.chunk.css"
+  },
+  {
+    "revision": "630c66d295ee7cf39405",
+    "url": "/mmss-frontend/static/js/2.b96c34fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mmss-frontend/static/js/2.b96c34fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "943c3d7f6c4efb67480e",
+    "url": "/mmss-frontend/static/js/main.ff273389.chunk.js"
+  },
+  {
+    "revision": "6ddb22ce32a271a9fb8c",
+    "url": "/mmss-frontend/static/js/runtime-main.5e680be2.js"
+  }
+]);
