7d956998306f162de56b8f7fcc20509fbcb52a93
Dreamufor <missmengxue@gmail.com>
2020-04-03 00:30:33 +1300

    fix form item value

--- src/components/Forms/CreateForm.js
+++ src/components/Forms/CreateForm.js
@@ -18,14 +18,14 @@ export default function CreateForm(props) {
       onCancel={props.handleCancel}
     >
       <Form {...layout}>
-        <Form.Item label="Type" name="">
+        <Form.Item label="Type" name="type">
           <Radio.Group defaultValue="withdraw">
             <Radio value="withdraw">Withdraw</Radio>
             <Radio value="transfer">Transfer</Radio>
             <Radio value="desposit">Desposit</Radio>
           </Radio.Group>
         </Form.Item>
-        <Form.Item label="Select account" name="">
+        <Form.Item label="Select account" name="account">
           <Select defaultValue="ANZ" style={{ width: 160 }}>
             <Select.Option value="ANZ">ANZ</Select.Option>
             <Select.Option value="Cash">Cash</Select.Option>
@@ -34,7 +34,7 @@ export default function CreateForm(props) {
           </Select>
         </Form.Item>
 
-        <Form.Item label="To" name="">
+        <Form.Item label="To" name="to">
           <Select defaultValue="ANZ" style={{ width: 160 }}>
             <Select.Option value="ANZ">ANZ</Select.Option>
             <Select.Option value="Cash">Cash</Select.Option>
@@ -42,26 +42,26 @@ export default function CreateForm(props) {
             <Select.Option value="Kiwibank">Kiwibank</Select.Option>
           </Select>
         </Form.Item>
-        <Form.Item label="Amount" name="">
+        <Form.Item label="Amount" name="amount">
           <InputNumber
             defaultValue={0}
             formatter={(value) => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
             parser={(value) => value.replace(/\$\s?|(,*)/g, '')}
           />
         </Form.Item>
-        <Form.Item label="Payee" name="">
+        <Form.Item label="Payee" name="payee">
           <Space>
             <Input style={{ width: 160 }} />
             <Button type="primary" shape="circle" icon={<PlusOutlined />} />
           </Space>
         </Form.Item>
-        <Form.Item label="Category" name="">
+        <Form.Item label="Category" name="category">
           <Space>
             <Input style={{ width: 160 }} />
             <Button type="primary" shape="circle" icon={<PlusOutlined />} />
           </Space>
         </Form.Item>
-        <Form.Item label="Note" name="">
+        <Form.Item label="Note" name="note">
           <Input.TextArea rows={3} style={{ width: 160 }} />
         </Form.Item>
       </Form>
