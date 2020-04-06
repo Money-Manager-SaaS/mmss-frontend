8034ec7d328a485c3b90e17e67c0ed5e3e262fdd
Dreamufor <missmengxue@gmail.com>
2020-04-06 13:58:33 +1200

    Updates

---
+++ precache-manifest.3bacb1c354883ede271df08b2463a910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23c63627918737e2be1bbe54567a70f",
+    "url": "/mmss-frontend/index.html"
+  },
+  {
+    "revision": "b0fca14f991327af007e",
+    "url": "/mmss-frontend/static/css/2.1eeb5667.chunk.css"
+  },
+  {
+    "revision": "fa7425d3a438fed1d6f1",
+    "url": "/mmss-frontend/static/css/main.db32a1d4.chunk.css"
+  },
+  {
+    "revision": "b0fca14f991327af007e",
+    "url": "/mmss-frontend/static/js/2.fae6bd2b.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/mmss-frontend/static/js/2.fae6bd2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7425d3a438fed1d6f1",
+    "url": "/mmss-frontend/static/js/main.b25fe7b5.chunk.js"
+  },
+  {
+    "revision": "6ddb22ce32a271a9fb8c",
+    "url": "/mmss-frontend/static/js/runtime-main.5e680be2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mmss-frontend/precache-manifest.85a2f6ef75484549c48507ad637aa66a.js"
+  "/mmss-frontend/precache-manifest.3bacb1c354883ede271df08b2463a910.js"
 );
 
 self.addEventListener('message', (event) => {
