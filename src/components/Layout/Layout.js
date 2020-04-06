7a8ddfd64c0239d0e607fd1b797f06f684f97835
Dreamufor <missmengxue@gmail.com>
2020-04-06 14:29:11 +1200

    update style

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,9 +5,7 @@ import { Layout as AntdLayout } from 'antd';
 
 export default function Layout(props) {
   return (
-    <AntdLayout
-      style={{ minHeight: '100vh', maxHeight: '100vh', backgroundColor: 'rgb(240, 242, 245)' }}
-    >
+    <AntdLayout style={{ minHeight: '100vh', backgroundColor: 'rgb(240, 242, 245)' }}>
       <SideBar />
       <UserInfo />
       <div>{props.children}</div>
