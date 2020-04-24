e61ff57cb9dca2396507712d2847b048b519b0f0
lichking <lichchen1983@gmail.com>
2020-04-25 09:31:58 +1200

    no css

--- src/views/auth/SignIn.js
+++ src/views/auth/SignIn.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react'
+import { Form, Input, Button} from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
-export default class componentName extends Component {
-    render() {
-        return (
-            <div>
-                <h1>signin</h1>
-            </div>
-        )
-    }
-}
+const Login= () => {
+  const onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+    ><Form.Item>
+      <h1>Sign In</h1>
+    </Form.Item>
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: 'Please input your name or Email!' }]}
+      ><p>Username/Email</p>
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your Password!' }]}
+      ><p>Password</p>
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+
+      <Form.Item>
+      <a href='register'>Haven't registed yet? sign now</a>
+      <br/>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+        
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Login
 
 
--- src/views/auth/SignUp.js
+++ src/views/auth/SignUp.js
@@ -1,11 +1,124 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
+import {Form,Input,Select,Row,Col,Checkbox,Button,} from 'antd';
 
-export default class componentName extends Component {
-    render() {
-        return (
-            <div>
-                sign up
-            </div>
-        )
-    }
-}
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+const Registe = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
+  return (
+
+    
+    
+    <Form
+      {...formItemLayout}
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      scrollToFirstError
+    >
+      <Form.Item><h1>Sign Up</h1></Form.Item>
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(rule, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+
+              return Promise.reject('The two passwords that you entered do not match!');
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item>
+        <Checkbox>
+          <a href="">I agree with terms and conditions</a>
+        </Checkbox>
+      </Form.Item>
+      <Form.Item><br/>
+        <a href='login'>Already have an account? Sign in here</a></Form.Item>
+
+      <Form.Item {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit">
+          Sign up
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Registe
