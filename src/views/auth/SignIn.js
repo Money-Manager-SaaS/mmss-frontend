0a6d277250cc14ebabd4f0abb1b133ab5e60802a
lichking <lichchen1983@gmail.com>
2020-04-25 09:24:41 +1200

    second

--- src/views/auth/SignIn.js
+++ src/views/auth/SignIn.js
@@ -1,2 +1,13 @@
-import React, { useState } from 'react';
-import { Tabs, Row, Col, Button } from 'antd';
+import React, { Component } from 'react'
+
+export default class componentName extends Component {
+    render() {
+        return (
+            <div>
+                <h1>signin</h1>
+            </div>
+        )
+    }
+}
+
+
