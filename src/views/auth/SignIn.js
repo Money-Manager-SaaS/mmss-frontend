64f130fe6a748bd54d647494da2832299f256960
kekakuka <39260972@qq.com>
2020-04-25 22:10:04 +1200

    mock login logout

--- src/views/auth/SignIn.js
+++ src/views/auth/SignIn.js
@@ -1,31 +1,42 @@
-import React, { Component } from 'react'
-import { Form, Input, Button} from 'antd';
+import React, { Component } from 'react';
+import { Form, Input, Button } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
-
-const Login= () => {
-  const onFinish = values => {
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setEmail } from '../../utils';
+import action from '@/store/action';
+const Login = ({ change_auth }) => {
+  const onFinish = (values) => {
     console.log('Received values of form: ', values);
   };
 
+  const mockLogin = (e) => {
+    e.preventDefault();
+    change_auth({ email: '39260972@qq.com', auth: true });
+    setEmail('39260972@qq.com');
+  };
   return (
     <Form
       name="normal_login"
       className="login-form"
       initialValues={{ remember: true }}
       onFinish={onFinish}
-    ><Form.Item>
-      <h1>Sign In</h1>
-    </Form.Item>
+    >
+      <Form.Item>
+        <h1>Sign In</h1>
+      </Form.Item>
       <Form.Item
         name="username"
         rules={[{ required: true, message: 'Please input your name or Email!' }]}
-      ><p>Username/Email</p>
+      >
+        <p>Username/Email</p>
         <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
       </Form.Item>
       <Form.Item
         name="password"
         rules={[{ required: true, message: 'Please input your Password!' }]}
-      ><p>Password</p>
+      >
+        <p>Password</p>
         <Input
           prefix={<LockOutlined className="site-form-item-icon" />}
           type="password"
@@ -34,17 +45,17 @@ const Login= () => {
       </Form.Item>
 
       <Form.Item>
-      <a href='register'>Haven't registed yet? sign now</a>
-      <br/>
+        <Link to="/register">Haven't registed yet? sign now</Link>
+        <br />
         <Button type="primary" htmlType="submit" className="login-form-button">
           Log in
         </Button>
-        
       </Form.Item>
+      <Button type="primary" htmlType="submit" className="login-form-button" onClick={mockLogin}>
+        Mock Login
+      </Button>
     </Form>
   );
 };
 
-export default Login
-
-
+export default connect(null, { change_auth: action.user.change_auth })(Login);
--- src/views/index.js
+++ src/views/index.js
@@ -3,4 +3,6 @@ import Profile from './profile';
 import Category from './category';
 import Payee from './payee';
 import Account from './account';
-export { Overall, Profile, Category, Account, Payee };
+import Login from './auth/SignIn';
+import Register from './auth/SignUp';
+export { Overall, Profile, Category, Account, Payee, Login, Register };
