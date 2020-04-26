381b5261b40905ad682449ba0cf41816d5d6c132
DonChan <don.chan@enterpriseit.co.nz>
2020-04-26 20:38:05 +1200

    - input event name: onChange (not onchange) - onChange receives a parameter: event. The value of the tag input is in event.target.value.

--- src/views/auth/SignIn.js
+++ src/views/auth/SignIn.js
@@ -36,7 +36,7 @@ const Login = ({ change_auth }) => {
         rules={[{ required: true, message: 'Please input your name or Email!' }]}
       >
         <p>Username/Email</p>
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" onchange={() => this.state.username = this.value } />
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" onChange={(e) => this.state.username = e.target.value } />
       </Form.Item>
       <Form.Item
         name="password"
@@ -47,7 +47,7 @@ const Login = ({ change_auth }) => {
           prefix={<LockOutlined className="site-form-item-icon" />}
           type="password"
           placeholder="Password"
-          onchange={() => this.state.password = this.value}
+          onChange={(e) => this.state.password = e.target.value}
         />
       </Form.Item>
 
