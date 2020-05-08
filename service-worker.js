3855ad5bdad13dd25fe0c137ecce44e7f00343dc
kekakuka <39260972@qq.com>
2020-05-08 22:49:20 +1200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ddf6441d743974187a3f4895d2be416.js"
+  "/precache-manifest.7ef728a5200ef3095d15bba52d88eb7f.js"
 );
 
 self.addEventListener('message', (event) => {
