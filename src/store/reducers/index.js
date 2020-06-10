3995963e7d9490de0a259c3e6f320af22bdfcd63
donchan <chendong.jy@gmail.com>
2020-06-10 18:32:46 +1200

    fix: delete transactions reducer from index.

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
-import transaction from './transaction';
 import account from './account';
 import category from './category';
 import user from './user';
@@ -11,7 +10,6 @@ import { reducer as toastrReducer } from 'react-redux-toastr';
 const reducer = (history) =>
   combineReducers({
     router: connectRouter(history),
-    transaction,
     account,
     category,
     user,
