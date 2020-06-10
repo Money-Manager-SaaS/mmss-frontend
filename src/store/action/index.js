4df0cab49640b751579d20cce1b6ccc1fa7d69d3
donchan <chendong.jy@gmail.com>
2020-06-10 17:25:49 +1200

    fix a bug: clean Transaction redux state bring in a bug. Fix this bug in this commit.

--- src/store/action/index.js
+++ src/store/action/index.js
@@ -1,9 +1,8 @@
-import transaction from './transaction';
 import account from './account';
 import category from './category';
 import user from './user';
 import payee from './payee';
 import globalLoading from './globalLoading';
-let action = { transaction, account, category, user, payee, globalLoading };
+let action = { account, category, user, payee, globalLoading };
 
 export default action;
